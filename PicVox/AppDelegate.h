0b1cb35d8467070973738a144e8bbfeadf77cb7a
bbu0810 <bbu0810@outlook.com>
2018-10-19 10:24:50 +1000

    version1.0.10.19

---
+++ PicVox/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PicVox
+//
+//  Created by Georgy Beckham on 5/17/17.
+//  Copyright © 2017 Georgy Beckham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
